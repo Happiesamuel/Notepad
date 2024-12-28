2050efa5ece4eb763aef7c1a0c8bdf28685e8122
Hs-the-dev <odioneghogho@gmail.com>
2024-12-28 15:39:46 +0100

    adjusted home

--- src/components/mobile/HomeScreen.tsx
+++ src/components/mobile/HomeScreen.tsx
@@ -12,10 +12,17 @@ export default function HomeScreen() {
     return dateB - dateA;
   });
   return (
-    <div className="h-full">
+    <div className="h-[80vh]">
       <div className="my-3 px-5 h-full ">
         {displayNote.length ? (
-          displayNote.map((note) => <DisplayNote note={note} key={note.id} />)
+          <>
+            <h1 className="text-xl font-semibold text-current-1 pb-2 ">
+              All notes
+            </h1>
+            {displayNote.map((note) => (
+              <DisplayNote note={note} key={note.id} />
+            ))}
+          </>
         ) : (
           <div className="flex flex-col text-current-2 items-center h-full justify-center">
             <div className="text-3xl bg-active rounded-full p-3">
